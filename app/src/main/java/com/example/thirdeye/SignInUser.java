af5775f548eae957c8e20a80bc66458edf37a927
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-10-14 17:14:25 +0600

    login done

--- app/src/main/java/com/example/thirdeye/SignInUser.java
+++ app/src/main/java/com/example/thirdeye/SignInUser.java
@@ -1,14 +1,76 @@
 package com.example.thirdeye;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SignInUser extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignInUser extends AppCompatActivity implements View.OnClickListener{
+    private Button signIn;
+    private TextInputLayout loginEmail,loginPass;
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_user);
+        signIn = findViewById(R.id.sign_in_sign);
+        signIn.setOnClickListener(this);
+
+        loginEmail = findViewById(R.id.emailLogin);
+        loginPass= findViewById(R.id.passwordLogin);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sign_in_sign:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email = loginEmail.getEditText().getText().toString().trim();
+        String password = loginPass.getEditText().getText().toString().trim();
+        if(email.isEmpty()){
+            loginEmail.setError("email is required.");
+            loginEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            loginEmail.setError("Please provide us a valid email.");
+            loginEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            loginPass.setError("Password is required.");
+            loginPass.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                 if(task.isSuccessful()){
+                      startActivity(new Intent(SignInUser.this, ProfileActivity.class));
+                 }else{
+                     Toast.makeText(SignInUser.this,"Failed to login! Please check your credentials",Toast.LENGTH_LONG).show();
+                 }
+            }
+        });
     }
 }
